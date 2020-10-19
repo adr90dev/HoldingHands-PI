a2328b482e34331668aa16ee17f7660efaa3e96b
unknown <brendo.cliber@gmail.com>
2020-10-19 18:13:24 -0300

    correção cascadeType usuario

--- HoldingHands/src/main/java/com/etcetera/HoldingHands/model/Usuario.java
+++ HoldingHands/src/main/java/com/etcetera/HoldingHands/model/Usuario.java
@@ -35,7 +35,7 @@ public class Usuario {
 	@Size(min = 8)
 	private String senha;
 	
-	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 	
